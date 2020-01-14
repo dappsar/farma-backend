6a76d3e1473442abfed4223ffe994a7f97b37704
Diego Baranowski <diego.baranowski@enerminds.com>
2020-01-14 00:35:53 -0300

    first commit

---
+++ build/precache-manifest.1bdb50e38da8ad65b76a7a95346a3418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e89cbd299874a56e17a491ed77554e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d1554dec6933d17c1f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e5ce14686c3786b3f2d9",
+    "url": "/static/js/2.fe95fdd8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fe95fdd8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84d1554dec6933d17c1f",
+    "url": "/static/js/main.174404f1.chunk.js"
+  },
+  {
+    "revision": "da518b15033f9777b08f",
+    "url": "/static/js/runtime-main.8f76e750.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
